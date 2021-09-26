56d62e13feaa0e59c47f360551e0cc08ea78a0a7
SergiiSamuylov <sergii.samuylov85@gmail.com>
2021-09-26 14:03:23 +0300

    Add user microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,23 @@
 Rails.application.routes.draw do
-  get 'password_resets/new'
+  get "password_resets/new"
 
-  get 'password_resets/edit'
+  get "password_resets/edit"
 
-  get 'sessions/new'
+  get "sessions/new"
 
-  get 'users/new'
+  get "users/new"
 
-  root                    'static_pages#home'
-  get       'help'     => 'static_pages#help'
-  get       'about'    => 'static_pages#about'
-  get       'mypage'   => 'static_pages#mypage'
-  get       'contact'  => 'static_pages#contact'
-  get       'signup'   => 'users#new'
-  get       'login'    => 'sessions#new'
-  post      'login'    => 'sessions#create'
-  delete    'logout'   => 'sessions#destroy'
+  root "static_pages#home"
+  get "help" => "static_pages#help"
+  get "about" => "static_pages#about"
+  get "mypage" => "static_pages#mypage"
+  get "contact" => "static_pages#contact"
+  get "signup" => "users#new"
+  get "login" => "sessions#new"
+  post "login" => "sessions#create"
+  delete "logout" => "sessions#destroy"
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :microposts, only: [:create, :destroy]
 end
