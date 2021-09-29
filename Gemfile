82646c940971ac001b1a414bbf9375fe09b45388
SergiiSamuylov <sergii.samuylov85@gmail.com>
2021-09-29 22:12:22 +0300

    Add user following

--- Gemfile
+++ Gemfile
@@ -17,6 +17,8 @@ gem 'turbolinks', '2.3.0'
 gem 'jbuilder', '2.2.3'
 gem 'sdoc', '0.4.0', group: :doc
 gem 'web-console', '2.0.0.beta3', group: :development
+gem 'net-ssh'
+gem 'xmlrpc'
 
 group :development, :test do
   gem 'sqlite3', '1.3.9'
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require File.expand_path('../boot', __FILE__)
+require File.expand_path("../boot", __FILE__)
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -22,5 +22,7 @@ module SampleApp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    # Include the authenticity token in remote forms.
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
